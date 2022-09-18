feee65d24b6bd978aa0856b7b9c77c60ec196888
tnsdnr0608 <tnsdnr1809@gmail.com>
2022-09-19 01:52:00 +0900

    drill.py

---
+++ 2019184037/drill4.py
@@ -0,0 +1,69 @@
+import turtle
+
+turtle.penup()
+turtle.goto(0,-200)
+turtle.pendown()
+
+count = 4
+while (count > 0):
+    turtle.forward(500)
+    turtle.left(90)
+    count = count - 1
+
+turtle.penup()
+turtle.goto(0,-100)
+turtle.pendown()
+turtle.forward(500)
+
+
+turtle.penup()
+turtle.goto(0,0)
+turtle.pendown()
+turtle.forward(500)
+
+turtle.penup()
+turtle.goto(0,100)
+turtle.pendown()
+turtle.forward(500)
+
+turtle.penup()
+turtle.goto(0,200)
+turtle.pendown()
+turtle.forward(500)
+
+turtle.penup()
+turtle.goto(0,300)
+turtle.pendown()
+turtle.forward(500)
+
+turtle.penup()
+turtle.goto(100,-200)
+turtle.left(90)
+turtle.pendown()
+turtle.forward(500)
+
+turtle.penup()
+turtle.goto(200,-200)
+turtle.pendown()
+turtle.forward(500)
+
+turtle.penup()
+turtle.goto(300,-200)
+turtle.pendown()
+turtle.forward(500)
+
+turtle.penup()
+turtle.goto(400,-200)
+turtle.pendown()
+turtle.forward(500)
+
+turtle.penup()
+turtle.goto(500,-200)
+turtle.pendown()
+turtle.forward(500)
+
+
+
+
+turtle.exitonclick()
+
