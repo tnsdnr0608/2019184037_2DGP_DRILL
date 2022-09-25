02b96332058b5f0933985ec2e1c409e26b2f4a86
tnsdnr0608 <tnsdnr1809@gmail.com>
2022-09-26 00:53:17 +0900

    humanmove

--- 2019184037/Drill6.2019184037/humanmove.py
+++ 2019184037/Drill6.2019184037/humanmove.py
@@ -1,41 +1,52 @@
-import math
 from pico2d import *
+import math
+
 
 open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x = 0
-while ( x < 800):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(x, 90)
-    x += 2
-    delay(0.01)
-y = 0
-while ( y < 800):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(750,y)
-    y += 2
-    delay(0.01)
+rectangle = 0
+turn = 0
+x, y = 400, 90
 
-x = 0
-while ( x < 800):
+while(1):
     clear_canvas_now()
     grass.draw_now(400,30)
-    character.draw_now(x, 550)
-    x += 2
-    delay(0.01)
 
-y = 0
-while ( y < 800):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(30,y)
-    y += 2
+    if turn % 2 == 0:
+        if rectangle % 4 == 0:
+            if x >= 800:
+                rectangle += 1
+            else:
+                x += 2
+
+        elif rectangle % 4 == 1:
+            if y >= 600:
+                rectangle += 1
+            else:
+                y += 2
+
+        elif rectangle % 4 == 2:
+            if x <= 0:
+                rectangle += 1
+            else:
+                x -= 2
+
+        else:
+            if y <= 90:
+                if x < 400:
+                    x += 2
+                else:
+                    rectangle += 1
+                    turn += 1
+
+            else:
+                y -= 2
+
+
+    character.draw_now(x, y)
     delay(0.01)
 
 close_canvas()
-
