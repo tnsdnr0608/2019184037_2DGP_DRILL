414c66b833823747a2de21b98e1f99b5d1f97ebd
tnsdnr0608 <tnsdnr1809@gmail.com>
2022-09-28 11:12:49 +0900

    수정 humanmove

--- 2019184037/Drill6.2019184037/humanmove2.py
+++ 2019184037/Drill6.2019184037/humanmove2.py
@@ -6,22 +6,38 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-def move():
-    math.sin(270 / 360 * 2 * math.pi)
-    
-x = 0
-y = 0
-while ( x < 800):
-    clear_canvas_now()
-    grass.draw_now(400,30)
-    character.draw_now(400,90)
-    delay(0.01)
+def render_all(x,y):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x, y)
+        delay(0.1)
 
-if x < '800':
-    clear_canvas_now()
-    grass.draw_now(move,y)
-    delay(0.01)
+def run_circle():
 
+    cx, cy, r = 400 , 300, 200
+    for deg in range(0, 360, 5):
+        x = cx + r * math.cos(deg / 360 * 2 * math.pi)
+        y = cy + r * math.sin(deg / 360 * 2 * math.pi)
+        render_all(x,y)
+
+def run_rectangle():
+    print('RECTANGLE')
+    for x in range(50, 750+1, 10):
+       render_all(x,90)
+    for y in range(90, 750+1, 10):
+        render_all(750,y)
+    for x in range(750, 50-1, -10):
+        render_all(x, 550)
+    for y in range(50, 750+1, 10):
+        render_all(50,y)
+        
+    pass
+
+
+while True:
+   # run_circle()
+    run_rectangle()
+    break
     
         
 close_canvas()
